7d3be10b234ccd5eb7166e2300ce7c793793f6f9
Olabamipe Taiwo <teeola48@gmail.com>
2020-05-01 00:09:09 +0000

    Updates

---
+++ precache-manifest.c23c87584bb10712b2a1e6cb2880cf79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9ca9992990e07971fc234bdf936551",
+    "url": "/bhl/index.html"
+  },
+  {
+    "revision": "8fb4f0e0dbda862f5457",
+    "url": "/bhl/static/css/main.9c0dfcfa.chunk.css"
+  },
+  {
+    "revision": "e84acafb025eaeaecfc9",
+    "url": "/bhl/static/js/2.f6d9ea26.chunk.js"
+  },
+  {
+    "revision": "8fb4f0e0dbda862f5457",
+    "url": "/bhl/static/js/main.08936cb0.chunk.js"
+  },
+  {
+    "revision": "10ebd0ecec6c2a85dcd8",
+    "url": "/bhl/static/js/runtime~main.1a219068.js"
+  },
+  {
+    "revision": "cd31d022e8d99ae0f647f777b72f69db",
+    "url": "/bhl/static/media/1.cd31d022.jpg"
+  },
+  {
+    "revision": "f78ca9cbc3d1f670c86db15c97038ac4",
+    "url": "/bhl/static/media/2.f78ca9cb.jpg"
+  },
+  {
+    "revision": "98e3df2ffdca13165a5c1c07613b7eb7",
+    "url": "/bhl/static/media/3.98e3df2f.jpg"
+  },
+  {
+    "revision": "cf78822d3ff1e8a1039722d2b0bd587a",
+    "url": "/bhl/static/media/4.cf78822d.jpg"
+  },
+  {
+    "revision": "0e55cf6f6adbff789a19aedbeaa82c0d",
+    "url": "/bhl/static/media/blc.0e55cf6f.png"
+  },
+  {
+    "revision": "f41f4fb78f1a99fb860592eca5e0b464",
+    "url": "/bhl/static/media/container.f41f4fb7.jpg"
+  },
+  {
+    "revision": "38147a220e4b5bf36ec328d32305148b",
+    "url": "/bhl/static/media/plane.38147a22.jpg"
+  },
+  {
+    "revision": "3f4b7142f47bd032a7118deab7fbe3c1",
+    "url": "/bhl/static/media/rec.3f4b7142.jpg"
+  }
+]);
